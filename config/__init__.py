af2050ec60a8c8a3a8462a9880d76269a17e63ef
fredygutierrezc-cpu <fredy.gutierrezc@istpargentina.edu.pe>
2026-07-17 13:50:35 -0500

    Update comments in __init__.py for clarity
    la configuracion de la base de tados  en SQlite

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,6 @@
 # Paquete de configuración
 # Este archivo hace que la carpeta 'config' sea reconocida como un paquete de Python
+# Contiene la configuracion de la base de datos 
 
 from config.conexcion import Conexion
 
