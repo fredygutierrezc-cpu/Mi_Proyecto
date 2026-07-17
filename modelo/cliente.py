7e0beb7ac8938963a7be421a2ab3a87fe34e41d5
fredygutierrezc-cpu <fredy.gutierrezc@istpargentina.edu.pe>
2026-07-17 13:46:41 -0500

    Refactor Cliente class attributes to private
    convertir  atributos  de la clase cliente a privado

--- modelo/cliente.py
+++ modelo/cliente.py
@@ -1,14 +1,11 @@
 class Cliente:
-    """Clase entidad que representa un Cliente"""
-
+    #Modelo/Entidad que representa un Cliente
+    
     def __init__(self, id_cliente=None, nombres=None, telefono=None, correo=None):
-        self.id_cliente = id_cliente
-        self.nombres = nombres
-        self.telefono = telefono
-        self.correo = correo
-
+        self._id_cliente = id_cliente
+        self._nombres = nombres
+        self._telefono = telefono
+        self._correo = correo
+    
     def __str__(self):
-        return f"Cliente(ID={self.id_cliente}, Nombre={self.nombres})"
-
-    def __repr__(self):
-        return self.__str__()
+        return f"Cliente(ID={self.id_cliente}, Nombre={self.nombres}, Tel={self.telefono}, Correo={self.correo})"
